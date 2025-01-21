e4cb1a80b7025d1d0265bf451905c2ad8d546ce9
Muhammed Mishab <mdmishab124@gmail.com>
2025-01-21 09:28:08 +0530

    Change Cors from Back-end for deffrent domain support

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ const limiter = rateLimit({
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://funoon-5rf3.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+  }));
+
 app.use(compression());
 app.use(helmet());
 app.use(morgan('dev'));
